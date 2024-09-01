d16b9d7f1af84d9185dfa78e0754304ec560ea47
lari-blip <anunciacaoteodor@gmail.com>
2024-09-01 16:44:15 -0300

    Lista 2

--- src/main/java/com/poo/listas/principal/lista2/exercicio5.java
+++ src/main/java/com/poo/listas/principal/lista2/exercicio5.java
@@ -1,5 +1,35 @@
 package com.poo.listas.principal.lista2;
 
-public class exercicio5 {
+import java.util.Scanner;
 
+public class exercicio5 {
+    public static void resolucao2() {
+       /* 5) Crie um algoritmo que receba login e senha e verifique as credenciais. Caso
+        algum deles estiver errado o programa deve retornar ao usuário quais das opções
+        está errada, se é o login ou a senha.*/
+
+        Scanner sc = new Scanner(System.in);
+
+        String login = "lari@gmail.com";
+        String senha = "la3006!@";
+        String verificaLogin;
+        String verificaSenha;
+
+        System.out.println("Entre com seu login: ");
+        verificaLogin = sc.nextLine();
+
+        System.out.println("Entre com sua senha: ");
+        verificaSenha = sc.nextLine();
+
+        if (!verificaLogin.equals(login)){
+            System.out.println("Login incorreto!");
+        }else if(!verificaSenha.equals(senha)){
+            System.out.println("Senha incorreta!");
+        }else {
+            System.out.println("Bem vindo ao sistema " + login);
+        }
+
+
+        sc.close();
+    }
 }
