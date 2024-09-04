c4c46977177529e6739411e9876da54c7954a8b2
lari-blip <anunciacaoteodor@gmail.com>
2024-09-04 15:02:24 -0300

    Lista 3

--- src/main/java/com/poo/listas/principal/lista3/exercicio1.java
+++ src/main/java/com/poo/listas/principal/lista3/exercicio1.java
@@ -2,27 +2,26 @@ package com.poo.listas.principal.lista3;
 
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-
-public class exercicio1{
-    public static void resolucao() throws InterruptedException {
-
-        int numeroDigitado;
-        Scanner leia = new Scanner(System.in);
-
-        do {
-            System.out.println("Digite um número em segundos:");
+    
+    public class exercicio1{
+        public static void resolucao3() throws InterruptedException {
+            
+            int numeroDigitado;
+    
+            System.out.println("Digite um número em segundos ente 1 e 59:");
+            Scanner leia = new Scanner(System.in);
             numeroDigitado = leia.nextInt();
-
-            if (numeroDigitado < 1  numeroDigitado > 59) {
-                System.out.println("Número inválido. Por favor, digite um número válido entre 1 e 59.");
-            }
-
-        } while (numeroDigitado < 1  numeroDigitado > 59);
-
-
+            
+            do {
+                System.out.println("Digite um número valido:");
+                numeroDigitado = leia.nextInt();  
+    
+        } while (numeroDigitado < 1 || numeroDigitado > 59);
+    
+    
         for (int i = 0; i <= numeroDigitado; i++) {
-            System.out.println( i );
-            TimeUnit.SECONDS.sleep(1);
+                System.out.println( "00:" + i );
+                TimeUnit.SECONDS.sleep(1);
         }
     }
 }
