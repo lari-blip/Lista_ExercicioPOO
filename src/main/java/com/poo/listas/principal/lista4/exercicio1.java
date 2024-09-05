7d1741d75f38c79f70bbb13dbb16620d3d6ea8d1
lari-blip <anunciacaoteodor@gmail.com>
2024-09-05 15:34:20 -0300

    Exercicio 1

---
+++ src/main/java/com/poo/listas/principal/lista4/exercicio1.java
@@ -0,0 +1,27 @@
+package com.poo.listas.principal.lista4;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void resolucao4(){
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Número de vendas do dia: ");
+        int numeroVendas = leia.nextInt();
+
+        double vendas[] = new double [numeroVendas];
+        double totalVendas = 0;
+
+        for (int i = 0; i <= numeroVendas; i++) {
+            System.out.println("Digite o valor da venda " + (i + 1) + ":");
+            vendas[i] = leia.nextDouble();
+        }
+        for (double venda : vendas) {
+            totalVendas += venda;
+        }
+        System.out.println("Total das vendas do dia: R$" + totalVendas);
+
+    leia.close();
+    }
+}
